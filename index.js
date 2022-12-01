d2a5460797fe6c543371ff047a4bf0258aaa3e86
Vishi2107 <119612975+Vishi2107@users.noreply.github.com>
2022-12-01 22:13:53 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -9,10 +9,13 @@ function updateClock()
     let m = new Date() . getMinutes()
     let s = new Date() . getSeconds()
     let ampm="AM"
+    if(h>=12)
+    {
+        ampm="PM";
+    }
     if(h>12)
     {
         h=h-12;
-        ampm="PM";
     }
     h= h<10 ? "0"+h:h;
     m= m<10 ? "0"+m:m;
@@ -21,7 +24,7 @@ function updateClock()
     hourE1.innerText=h;
     minuteE1.innerText=m;
     secondE1.innerText=s;
-    ampmE1, (innerText=ampm);
+    ampmE1.innerText=ampm;
     setTimeout(()=>
     {
         updateClock()
